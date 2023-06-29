e404947e9eb3115a08a0010da14937b5b902b5f2
Oleg Solovyov <mcpain@altlinux.org>
2023-06-29 18:54:06 +0300

    Use glm

--- tracer.cpp
+++ tracer.cpp
@@ -8,28 +8,24 @@
 static long int s_time;
 static struct tm m_time;
 
-struct V {
-  float x, y, z;
-  V(float v = 0) { x = y = z = v; }
-  V(float a, float b, float c = 0) {
-    x = a;
-    y = b;
-    z = c;
-  }
-  V operator + (V r) { return V(x + r.x, y + r.y, z + r.z); }
-  V operator *(V r) { return V(x * r.x, y * r.y, z * r.z); }
-  float operator % (V r) { return x *r.x + y *r.y + z *r.z; }
-  V operator !() { return *this *(1 / sqrtf(*this % *this)); }
-};
+#include <glm/glm.hpp>
+
+float operator% (glm::vec3 a, glm::vec3 b) {
+  return glm::dot(a, b);
+}
+glm::vec3 operator! (glm::vec3 a) {
+  return glm::normalize(a);
+}
+
 float L(float l, float r) { return l < r ? l : r; }
 float U() { return(float) rand() / RAND_MAX; }
-float B(V p, V l, V h) {
-  l = p + l * -1;
-  h = h + p * -1;
+float B(glm::vec3 p, glm::vec3 l, glm::vec3 h) {
+  l = p - l;
+  h = h - p;
   return -L(L(L(l.x, h.x), L(l.y, h.y)), L(l.z, h.z));
 }
 
-static V position = V(0);
+static glm::vec3 position = glm::vec3(0);
 
 bool sevenSegTest(int digit, int segment) {
   switch (segment) {
@@ -59,9 +55,9 @@ bool sevenSegTest(int digit, int segment) {
   }
 }
 
-float S(V p, int &m) {
+float S(glm::vec3 p, int &m) {
   float d = 1e9;
-  V f = p;
+  glm::vec3 f = p;
   f.z = 0;
 
   char l[] =
@@ -93,27 +89,27 @@ float S(V p, int &m) {
     }
     for (int i = 0; i < 28; i += 4) {
       if (sevenSegTest(digit, i / 4)) {
-        V b = V(l[i] - 79 + offsets[j], l[i + 1] - 79) * .5,
-          e = V(l[i + 2] - 79 + offsets[j], l[i + 3] - 79) * .5 + b * -1,
-          o = f + (b + e * L(-L((b + f * -1) % e / (e % e), 0), 1)) * -1;
+        glm::vec3 b = glm::vec3(l[i] - 79 + offsets[j], l[i + 1] - 79, 0) * .5f,
+          e = glm::vec3(l[i + 2] - 79 + offsets[j], l[i + 3] - 79, 0) * .5f - b,
+          o = f - (b + e * L(-L((b - f) % e / (e % e), 0), 1));
         d = L(d, o % o);
       }
     }
   }
   char ll[] = "MSMS" "M[M["; // hhmm separator
   for (int i = 0; i < 8; i += 4) {
-    V b = V(ll[i] - 79, ll[i + 1] - 79) * .5,
-      e = V(ll[i + 2] - 79, ll[i + 3] - 79) * .5 + b * -1,
-      o = f + (b + e * L(-L((b + f * -1) % e / (e % e), 0), 1)) * -1;
+    glm::vec3 b = glm::vec3(ll[i] - 79, ll[i + 1] - 79, 0) * .5f,
+      e = glm::vec3(ll[i + 2] - 79, ll[i + 3] - 79, 0) * .5f - b,
+      o = f - (b + e * L(-L((b - f) % e / (e % e), 0), 1));
     d = L(d, o % o);
   }
   d = sqrtf(d);
   d = powf(powf(d, 8) + powf(p.z, 8), .125) - .5;
   m = 1;
   float r = L(
-          -L(B(p, V(-30, -.5, -30), V(30, 18, 30)),
-             B(p, V(-25, 17, -25), V(25, 20, 25))),
-          B(V(fmodf(fabsf(p.x), 8), p.y, p.z), V(1.5, 18.5, -25), V(6.5, 20, 25)));
+          -L(B(p, glm::vec3(-30, -.5, -30), glm::vec3(30, 18, 30)),
+             B(p, glm::vec3(-25, 17, -25), glm::vec3(25, 20, 25))),
+          B(glm::vec3(fmodf(fabsf(p.x), 8), p.y, p.z), glm::vec3(1.5, 18.5, -25), glm::vec3(6.5, 20, 25)));
   if (r < d)
     d = r, m = 2;
   float s = 9 - p.y;
@@ -121,13 +117,13 @@ float S(V p, int &m) {
     d = s, m = 3;
   return d;
 }
-int M(V o, V d, V &h, V &n) {
+int M(glm::vec3 o, glm::vec3 d, glm::vec3 &h, glm::vec3 &n) {
   int m, s = 0;
   float t = 0, c;
   for (; t < 100; t += c)
     if ((c = S(h = o + d * t, m)) < .01 || ++s > 99)
-      return n = !V(S(h + V(.01, 0), s) - c, S(h + V(0, .01), s) - c,
-                    S(h + V(0, 0, .01), s) - c),
+      return n = !glm::vec3(S(h + glm::vec3(.01, 0, 0), s) - c, S(h + glm::vec3(0, .01, 0), s) - c,
+                    S(h + glm::vec3(0, 0, .01), s) - c),
              m;
   return 0;
 }
@@ -136,8 +132,8 @@ int M(V o, V d, V &h, V &n) {
 bool solveQuadratic(float a, float b, float c, float& x1, float& x2)
 {
     if (b == 0) {
-        // Handle special case where the the two vector ray.dir and V are perpendicular
-        // with V = ray.orig - sphere.centre
+        // Handle special case where the the two vector ray.dir and glm::vec3 are perpendicular
+        // with glm::vec3 = ray.orig - sphere.centre
         if (a == 0) return false;
         x1 = 0; x2 = sqrtf(-c / a);
         return true;
@@ -152,7 +148,7 @@ bool solveQuadratic(float a, float b, float c, float& x1, float& x2)
 
     return true;
 }
-bool raySphereIntersect(const V& orig, const V& dir, const float& radius, float& t0, float& t1)
+bool raySphereIntersect(const glm::vec3& orig, const glm::vec3& dir, const float& radius, float& t0, float& t1)
 {
     // They ray dir is normalized so A = 1
     float A = dir.x * dir.x + dir.y * dir.y + dir.z * dir.z;
@@ -165,11 +161,11 @@ bool raySphereIntersect(const V& orig, const V& dir, const float& radius, float&
 
     return true;
 }
-V computeIncidentLight(V sunDirection, V orig, V dir, float tmin, float tmax)
+glm::vec3 computeIncidentLight(glm::vec3 sunDirection, glm::vec3 orig, glm::vec3 dir, float tmin, float tmax)
 {
     sunDirection = !sunDirection;
-    V betaR(3.8e-6f, 13.5e-6f, 33.1e-6f);
-    V betaM(21e-6f);
+    glm::vec3 betaR(3.8e-6f, 13.5e-6f, 33.1e-6f);
+    glm::vec3 betaM(21e-6f);
     float earthRadius = 6360e3,
       atmosphereRadius = 6420e3,
       Hr = 7994,
@@ -177,21 +173,21 @@ V computeIncidentLight(V sunDirection, V orig, V dir, float tmin, float tmax)
 
     orig.y += earthRadius;
     float t0, t1;
-    if (!raySphereIntersect(orig, dir, atmosphereRadius, t0, t1) || t1 < 0) return 0;
+    if (!raySphereIntersect(orig, dir, atmosphereRadius, t0, t1) || t1 < 0) return glm::vec3(0.f);
     if (t0 > tmin && t0 > 0) tmin = t0;
     if (t1 < tmax) tmax = t1;
     int numSamples = 16;
     int numSamplesLight = 8;
     float segmentLength = (tmax - tmin) / numSamples;
     float tCurrent = tmin;
-    V sumR(0), sumM(0); // mie and rayleigh contribution
+    glm::vec3 sumR(0), sumM(0); // mie and rayleigh contribution
     float opticalDepthR = 0, opticalDepthM = 0;
     float mu = dir % sunDirection; // mu in the paper which is the cosine of the angle between the sun direction and the ray direction
     float phaseR = 3.f / (16.f * 3.141592) * (1 + mu * mu);
     float g = 0.76f;
     float phaseM = 3.f / (8.f * 3.141592) * ((1.f - g * g) * (1.f + mu * mu)) / ((2.f + g * g) * pow(1.f + g * g - 2.f * g * mu, 1.5f));
     for (int i = 0; i < numSamples; ++i) {
-        V samplePosition = orig + V(tCurrent + segmentLength * 0.5f) * dir;
+        glm::vec3 samplePosition = orig + glm::vec3(tCurrent + segmentLength * 0.5f) * dir;
         float height = sqrtf(samplePosition % samplePosition) - earthRadius;
         // compute optical depth for light
         float hr = exp(-height / Hr) * segmentLength;
@@ -205,7 +201,7 @@ V computeIncidentLight(V sunDirection, V orig, V dir, float tmin, float tmax)
         float opticalDepthLightR = 0, opticalDepthLightM = 0;
         int j;
         for (j = 0; j < numSamplesLight; ++j) {
-            V samplePositionLight = samplePosition + V(tCurrentLight + segmentLengthLight * 0.5f) * sunDirection;
+            glm::vec3 samplePositionLight = samplePosition + glm::vec3(tCurrentLight + segmentLengthLight * 0.5f) * sunDirection;
             float heightLight = sqrtf(samplePositionLight % samplePositionLight) - earthRadius;
             if (heightLight < 0) break;
             opticalDepthLightR += exp(-heightLight / Hr) * segmentLengthLight;
@@ -213,8 +209,8 @@ V computeIncidentLight(V sunDirection, V orig, V dir, float tmin, float tmax)
             tCurrentLight += segmentLengthLight;
         }
         if (j == numSamplesLight) {
-            V tau = betaR * (opticalDepthR + opticalDepthLightR) + betaM * 1.1f * (opticalDepthM + opticalDepthLightM);
-            V attenuation(exp(-tau.x), exp(-tau.y), exp(-tau.z));
+            glm::vec3 tau = betaR * (opticalDepthR + opticalDepthLightR) + betaM * 1.1f * (opticalDepthM + opticalDepthLightM);
+            glm::vec3 attenuation(exp(-tau.x), exp(-tau.y), exp(-tau.z));
             sumR = sumR + attenuation * hr;
             sumM = sumM + attenuation * hm;
         }
@@ -225,12 +221,12 @@ V computeIncidentLight(V sunDirection, V orig, V dir, float tmin, float tmax)
     // We use a magic number here for the intensity of the sun (20). We will make it more
     // scientific in a future revision of this lesson/code
     // [/comment]
-    V ret = (sumR * betaR * phaseR + sumM * betaM * phaseM);
+    glm::vec3 ret = (sumR * betaR * phaseR + sumM * betaM * phaseM);
     return ret;
 }
 
 void calculateSunPosition() {
-  position = V(0, 0, 1); // x+: west, y+: above horizon, z+: north, initial is south, looking at SW
+  position = glm::vec3(0, 0, 1); // x+: west, y+: above horizon, z+: north, initial is south, looking at SW
 
   float x1, y1, z1;
 
@@ -268,15 +264,15 @@ void calculateSunPosition() {
 }
 
 
-V T(V o, V d) {
-  V h, n, r, t = 1;
+glm::vec3 T(glm::vec3 o, glm::vec3 d) {
+  glm::vec3 h, n, r = glm::vec3(0.f), t = glm::vec3(1.f);
 
   // light sources
-  V ls[] = {
+  glm::vec3 ls[] = {
     position,
   };
-  V cs[] = {
-    V(500, 500, 500),
+  glm::vec3 cs[] = {
+    glm::vec3(500, 500, 500),
   };
   int w = 1;
 
@@ -286,25 +282,25 @@ V T(V o, V d) {
       break;
     if (m == 1) {
       d = d + n * (n % d * -2);
-      o = h + d * .1;
-      t = t * .2;
+      o = h + d * .1f;
+      t = t * .2f;
 
       // glowing letters?
-      V letter_color;
+      glm::vec3 letter_color;
       int season = ((m_time.tm_mon + 1) % 12) / 3; // 0-11 -> 0-3
 
       switch (season) {
         case 0: // winter
-          letter_color = V(0, 5, 5);
+          letter_color = glm::vec3(0, 5, 5);
           break;
         case 1: // spring
-          letter_color = V(0, 5, 0);
+          letter_color = glm::vec3(0, 5, 0);
           break;
         case 2: // summer
-          letter_color = V(5, 5, 0);
+          letter_color = glm::vec3(5, 5, 0);
           break;
         case 3: // fall
-          letter_color = V(5, 1, 0);
+          letter_color = glm::vec3(5, 1, 0);
           break;
       }
       r = r + letter_color;
@@ -313,23 +309,23 @@ V T(V o, V d) {
       if (U() < 0) {
         // specular reflection
         d = d + n * (n % d * -2);
-        o = h + d * .1;
-        t = t * .2;
+        o = h + d * .1f;
+        t = t * .2f;
       } else {
         // diffuse reflection
         float p = 6.283185 * U(), c = U(), s = sqrtf(1 - c),
           g = n.z < 0 ? -1 : 1, u = -1 / (g + n.z), v = n.x * n.y * u;
-        d = V(v, g + n.y * n.y * u, -n.y) * (cosf(p) * s) +
-            V(1 + g * n.x * n.x * u, g * v, -g * n.x) * (sinf(p) * s) +
+        d = glm::vec3(v, g + n.y * n.y * u, -n.y) * (cosf(p) * s) +
+            glm::vec3(1 + g * n.x * n.x * u, g * v, -g * n.x) * (sinf(p) * s) +
             n * sqrtf(c);
-        o = h + d * .1;
-        t = t * .2;
+        o = h + d * .1f;
+        t = t * .2f;
 
         for (int a = 0; a < w; a++) {
-          V l = !ls[a], hc = h, nc = n;
+          glm::vec3 l = !ls[a], hc = h, nc = n;
           float i = n % l;
-          if (i > 0 && M(h + n * .1, l, hc, nc) == 3) {
-            V incident = computeIncidentLight(l, o, l, 0, 1e9);
+          if (i > 0 && M(h + n * .1f, l, hc, nc) == 3) {
+            glm::vec3 incident = computeIncidentLight(l, o, l, 0, 1e9);
             r = r + t * incident * cs[a] * i;
           }
         }
@@ -337,7 +333,7 @@ V T(V o, V d) {
     }
     if (m == 3) {
       for (int a = 0; a < w; a++) {
-        V l = !ls[a];
+        glm::vec3 l = !ls[a];
         r = r + t * computeIncidentLight(l, o, d, 0, 1e9) * cs[a];
       }
       break;
@@ -355,26 +351,26 @@ int main() {
   localtime_r (&s_time, &m_time);
   calculateSunPosition();
 
-  int w = 1920, h = 1080, s = 4096;
-  V e(-22, 5, 25),
-      g = !(V(-3, 4, 0) + e * -1), l = !V(g.z, 0, -g.x) * (1. / w),
+  int w = 1920, h = 1080, s = 1;
+  glm::vec3 e(-22, 5, 25),
+      g = !(glm::vec3(-3, 4, 0) - e), l = !glm::vec3(g.z, 0, -g.x) * (1.f / w),
       u(g.y * l.z - g.z * l.y, g.z * l.x - g.x * l.z, g.x * l.y - g.y * l.x);
   fprintf(f, "P6 %d %d 255 ",
           w, h);
   for (int y = h; y--;) {
     printf("Rendered %d rows out of %d\n", y, h);
     for (int x = w; x--;) {
-      V c;
+      glm::vec3 c(0.f);
       int p;
       for (p = s; p > 0; p--)
         c = c + T(e, !(g + l * (x - w / 2 + U()) + u * (y - h / 2 + U())));
-      c = c * (1. / s) + 14. / 241;
-      V o = c + 1;
-      c = V(c.x / o.x, c.y / o.y, c.z / o.z);
+      c = c * (1.f / s) + 14.f / 241;
+      glm::vec3 o = c + 1.f;
+      c = glm::vec3(c.x / o.x, c.y / o.y, c.z / o.z);
 
       // gamma correction
       float gamma = 2.2;
-      c = V(powf(c.x, gamma), powf(c.y, gamma), powf(c.z, gamma)) * 255;
+      c = glm::vec3(powf(c.x, gamma), powf(c.y, gamma), powf(c.z, gamma)) * 255.f;
 
       fprintf(f, "%c%c%c", (int)c.x, (int)c.y, (int)c.z);
     }
